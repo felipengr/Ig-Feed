800d504b93a6e1023785ce7f353d0ee881b8f3ea
Felipe Nogueira <felipenogueira.94@gmail.com>
2023-01-23 22:12:45 -0300

    estilizando componente sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { Header } from "./components/Header";
 import { Post } from "./Post";
+import { Sidebar } from "./components/Sidebar";
 
 import './global.css'
+import styles from './App.module.css'
 
  export function App() {
 
@@ -9,8 +11,13 @@ import './global.css'
     <>
       <Header />
 
-      <Post author={'Felipe Nogueira'} content={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut non lorem orci. Nam aliquet ante ac iaculis dignissim. Aliquam eu urna a metus auctor interdum vitae in mauris. Praesent commodo aliquam feugiat. Lorem ipsum dolor sit amet, consectetur adipiscing elit.'} />
-      <Post author={'Fernanda Campagnolo'} content={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut non lorem orci. Nam aliquet ante ac iaculis dignissim. Aliquam eu urna a metus auctor interdum vitae in mauris. Praesent commodo aliquam feugiat. Lorem ipsum dolor sit amet, consectetur adipiscing elit.'} />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post author={'Felipe Nogueira'} content={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut non lorem orci. Nam aliquet ante ac iaculis dignissim. Aliquam eu urna a metus auctor interdum vitae in mauris. Praesent commodo aliquam feugiat. Lorem ipsum dolor sit amet, consectetur adipiscing elit.'} />
+          <Post author={'Fernanda Campagnolo'} content={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut non lorem orci. Nam aliquet ante ac iaculis dignissim. Aliquam eu urna a metus auctor interdum vitae in mauris. Praesent commodo aliquam feugiat. Lorem ipsum dolor sit amet, consectetur adipiscing elit.'} />
+        </main>
+      </div>
     </>
   )
 }
