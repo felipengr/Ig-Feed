6d24e347c46c69f2bc3aaa3f3dab75fdb3df3395
Felipe Nogueira <felipenogueira.94@gmail.com>
2023-01-23 21:22:28 -0300

    aula css modules

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Header } from "./components/Header";
+import { Post } from "./Post";
+
+import './styles.css'
+
+ export function App() {
+
+  return (
+    <>
+      <Header />
+
+      <Post author={'Felipe Nogueira'} content={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut non lorem orci. Nam aliquet ante ac iaculis dignissim. Aliquam eu urna a metus auctor interdum vitae in mauris. Praesent commodo aliquam feugiat. Lorem ipsum dolor sit amet, consectetur adipiscing elit.'} />
+      <Post author={'Fernanda Campagnolo'} content={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut non lorem orci. Nam aliquet ante ac iaculis dignissim. Aliquam eu urna a metus auctor interdum vitae in mauris. Praesent commodo aliquam feugiat. Lorem ipsum dolor sit amet, consectetur adipiscing elit.'} />
+    </>
+  )
+}
