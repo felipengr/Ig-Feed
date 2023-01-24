0c03054b4dfcad03d348663bf55736d7035cfd07
Felipe Nogueira <felipenogueira.94@gmail.com>
2023-01-23 22:39:10 -0300

    finalizando sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from 'phosphor-react'
+
 import styles from './Sidebar.module.css'
 
 export function Sidebar() {
@@ -7,12 +9,15 @@ export function Sidebar() {
                 <img className={styles.cover} src="https://images.unsplash.com/photo-1546146830-2cca9512c68e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=50" />
 
                 <div className={styles.profile}>
+                    <img className={styles.avatar} src="https://github.com/felipengr.png" />
+
                    <strong>Felipe Nogueira</strong> 
                    <span>Web Developer</span>
                 </div>
 
                 <footer>
                     <a href="#">
+                        <PencilLine size={20} />
                         Editar seu perfil
                     </a> 
                 </footer>
