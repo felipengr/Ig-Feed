60d4f31d7c8b939bb4d8031d416482bd88c7a775
Felipe Nogueira <felipenogueira.94@gmail.com>
2023-01-24 20:43:09 -0300

    componente: Post

---
+++ src/components/Post.jsx
@@ -0,0 +1,24 @@
+import styles from './Post.module.css'
+
+export function Post () {
+    return (
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img className={styles.avatar} src="https://github.com/diego3g.png" />
+                    <div className={styles.authorInfo}>
+                        <strong>Diego Fernandes</strong>
+                        <span>Web Developer</span>
+                    </div>
+                </div>
+                <time title='24 de janeiro às 19h36' dateTime='2023-01-24 19:36:48'>Publicado há 1h</time>
+            </header>
+            <div className={styles.content}>
+                <p>Fala galeraa 👋</p>
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+                <p><a href='#'>👉 jane.design/doctorcare</a></p>
+                <p><a href='#'>#novoprojeto #nlw #rocketseat</a></p>
+            </div>
+        </article>
+    )
+}
