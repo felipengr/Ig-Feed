86fd82f1b4541859d08964df6d6214db4677feab
Felipe Nogueira <felipenogueira.94@gmail.com>
2023-01-24 22:22:15 -0300

    Componente avatar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react'
+import { Avatar } from './Avatar';
 
 import styles from './Sidebar.module.css'
 
@@ -9,7 +10,7 @@ export function Sidebar() {
                 <img className={styles.cover} src="https://images.unsplash.com/photo-1546146830-2cca9512c68e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=50" />
 
                 <div className={styles.profile}>
-                    <img className={styles.avatar} src="https://github.com/felipengr.png" />
+                    <Avatar src="https://github.com/felipengr.png" />
 
                    <strong>Felipe Nogueira</strong> 
                    <span>Web Developer</span>
