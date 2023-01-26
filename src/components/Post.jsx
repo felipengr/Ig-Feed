2c2493512fdebb59bb567333f02e52713ee24b75
Felipe Nogueira <felipenogueira.94@gmail.com>
2023-01-26 19:32:01 -0300

    Estado (useState)

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,11 +1,19 @@
+import { useState } from 'react'
 import { format, formatDistanceToNow } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
 
 import { Avatar } from './Avatar'
 import { Comment } from './Comment'
+
 import styles from './Post.module.css'
 
+
 export function Post ({ author, publishedAt, content }) {
+    const [comments, setComments] = useState([
+        1,
+        2
+    ])
+
     const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
         locale: ptBR,
     });
@@ -15,6 +23,11 @@ export function Post ({ author, publishedAt, content }) {
         addSuffix: true
     })
 
+    function handleCreateNewComment() {
+        event.preventDefault()
+
+        setComments([...comments, comments.length + 1]);
+    }
 
     return (
         <article className={styles.post}>
@@ -39,7 +52,7 @@ export function Post ({ author, publishedAt, content }) {
                     }
                 })}
             </div>
-            <form className={styles.commentForm}>
+            <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
                 <strong>Deixe seu feedback</strong>
                 <textarea 
                     placeholder='Deixe um comentário'
@@ -50,9 +63,9 @@ export function Post ({ author, publishedAt, content }) {
             </form>
 
             <div className={styles.commentList}>
-                <Comment />
-                <Comment />
-                <Comment />
+                {comments.map(comment => {
+                    return <Comment />
+                })}
             </div>
         </article>
     )
